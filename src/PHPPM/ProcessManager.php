72cf6c759ae1adcddbc1f63d758fce321287b82c
Rixbeck <rix@neologik.hu>
2018-06-30 15:51:41 +0200

    Implementing live reload of config files

--- src/PHPPM/ProcessManager.php
+++ src/PHPPM/ProcessManager.php
@@ -1,14 +1,70 @@
 <?php
-declare(ticks = 1);
+declare(ticks=1);
 
 namespace WD40\PHPPM;
 
 use PHPPM\Slave;
-use Symfony\Component\Process\ProcessUtils;
 use React\ChildProcess\Process;
+use React\EventLoop\Factory;
+use React\Socket\Server;
+use React\Socket\UnixServer;
+use ReactFilesystemMonitor\FilesystemMonitorFactory;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\Process\ProcessUtils;
 
 class ProcessManager extends \PHPPM\ProcessManager
 {
+    protected $configsChanged = 0;
+
+    /**
+     * @throws \ReflectionException
+     */
+    public function run()
+    {
+        Debug::enable();
+
+        // make whatever is necessary to disable all stuff that could buffer output
+        ini_set('zlib.output_compression', 0);
+        ini_set('output_buffering', 0);
+        ini_set('implicit_flush', 1);
+        ob_implicit_flush(1);
+
+        $this->loop = Factory::create();
+        $this->controller = new UnixServer($this->getControllerSocketPath(), $this->loop);
+        $this->controller->on('connection', [$this, 'onSlaveConnection']);
+
+        $this->web = new Server(sprintf('%s:%d', $this->host, $this->port), $this->loop);
+        $this->web->on('connection', [$this, 'onRequest']);
+
+        $pcntl = new \MKraemer\ReactPCNTL\PCNTL($this->loop);
+        $pcntl->on(SIGTERM, [$this, 'shutdown']);
+        $pcntl->on(SIGINT, [$this, 'shutdown']);
+        $pcntl->on(SIGCHLD, [$this, 'handleSigchld']);
+        $pcntl->on(SIGUSR1, [$this, 'restartSlaves']);
+        $pcntl->on(SIGUSR2, [$this, 'reloadSlaves']);
+
+        if ($this->isDebug()) {
+            $this->loop->addPeriodicTimer(0.5, function () {
+                $this->checkChangedFiles();
+            });
+        }
+
+        $loopClass = (new \ReflectionClass($this->loop))->getShortName();
+
+        $this->output->writeln("<info>Starting PHP-PM with {$this->slaveCount} workers, using {$loopClass} ...</info>");
+        $this->writePid();
+
+        $this->createSlaves();
+
+        $this->loop->addPeriodicTimer(2, function () {
+            $this->checkChangedConfigs();
+        });
+
+        $this->fsMonitorStart();
+
+        $this->loop->run();
+    }
+
     /**
      * @param int $port
      * @throws \Exception
@@ -42,7 +98,7 @@ class ProcessManager extends \PHPPM\ProcessManager
 
         $config = var_export($config, true);
 
-        $dir = var_export(__DIR__ . '/../..', true);
+        $dir = var_export(__DIR__.'/../..', true);
         $script = <<<EOF
 <?php
 
@@ -84,7 +140,7 @@ EOF;
         // e.g. headers_sent() returns always true, although wrong.
         //For version 2.x and 3.x of \Symfony\Component\Process\Process package
         if (method_exists('\Symfony\Component\Process\ProcessUtils', 'escapeArgument')) {
-            $commandline = 'exec ' . $this->phpCgiExecutable . ' -C ' . ProcessUtils::escapeArgument($file);
+            $commandline = 'exec '.$this->phpCgiExecutable.' -C '.ProcessUtils::escapeArgument($file);
         } else {
             //For version 4.x of \Symfony\Component\Process\Process package
             $commandline = ['exec', $this->phpCgiExecutable, '-C', $file];
@@ -110,5 +166,35 @@ EOF;
                 $this->output->write("<error>$data</error>");
             }
         );
+    }
+
+    protected function fsMonitorStart()
+    {
+        $watchDir = realpath(__DIR__.'/../../app/config/');
+        $monitor = (new FilesystemMonitorFactory())->create($watchDir, ['modify']);
+
+        $monitor->on('all', function ($path, $isDir, $event, $monitor) {
+
+            $this->output->writeln(
+                sprintf("FS '%s' event occures at:  %s%s", $event, $path, $isDir ? ' [dir]' : '')
+            );
+
+            $this->configsChanged++;
+        });
+
+        $monitor->start($this->loop);
+    }
+
+    protected function checkChangedConfigs()
+    {
+        if (!$this->configsChanged) {
+            return;
+        }
+
+        $this->output->writeln(
+            sprintf("Restarting workers...")
+        );
+        $this->restartSlaves();
+        $this->configsChanged = 0;
     }
 }
